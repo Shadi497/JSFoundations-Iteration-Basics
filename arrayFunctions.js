e872c6fdc4afceb3568386aec22507c5e5fa2d3e
Shadi497 <b.shadi.h@gmail.com>
2021-01-12 09:29:28 +0300

    Finished

--- arrayFunctions.js
+++ arrayFunctions.js
@@ -8,9 +8,12 @@
  * getOdds([1, 2, 3, 4, 5, 6, 7, 8, 9]) -> [1, 3, 5, 7, 9]
  * getOdds([11, 35, 52, 14, 56, 601, 777, 888, 999]) -> [11, 35, 601, 777, 999]
  */
+
 function getOdds(numbers) {
-  // Your code here
-}
+ return numbers.filter(function(number){
+    return number%2!==0
+  });
+  }
 
 /**
  * getEvens(numbers):
@@ -23,8 +26,9 @@ function getOdds(numbers) {
  * getEvens([11, 35, 52, 14, 56, 601, 777, 888, 999]) -> [52, 14, 56, 888]
  */
 function getEvens(numbers) {
-  // Your code here
-}
+ return numbers.filter(function(number){
+    return number%2===0
+  });}
 
 /**
  * countOccurences(x, numbers):
@@ -36,7 +40,11 @@ function getEvens(numbers) {
  * countOccurences(52, [11, 35, 52, 14, 56, 601, 52, 777, 888, 999, 52]) -> 3
  */
 function countOccurences(x, numbers) {
-  // Your code here
+  let count =0;
+  numbers.forEach(function(number){
+    if(number===x) count++;
+  });
+  return count;
 }
 
 /**
@@ -49,7 +57,9 @@ function countOccurences(x, numbers) {
  */
 
 function makeThemDoctors(students) {
-  // Your code here
+ return students.map(function(student){
+  return `Dr. ${student}`;
+ });
 }
 
 module.exports = { getOdds, getEvens, countOccurences, makeThemDoctors };
